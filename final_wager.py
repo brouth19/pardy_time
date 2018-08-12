92edf76b0d102d10a1ef1020db76e277afe4d8ac
brouth19 <brouth19@amherst.edu>
2018-08-12 15:56:26 -0400

    implementation of bucketized feature columns and L1 Regularization

--- final_wager.py
+++ final_wager.py
@@ -30,13 +30,11 @@ def preprocess_features(jeopardy_dataframe):
        synthetic features.
      """
 
+    #previously included "incorrect_a","incorrect_b","incorrect_c"
     selected_features = jeopardy_dataframe[
     ["correct_a",
      "correct_b",
      "correct_c",
-     "incorrect_a",
-     "incorrect_b",
-     "incorrect_c",
      "num_since_correct_a",
      "num_since_correct_b",
      "num_since_correct_c",
@@ -57,9 +55,9 @@ def preprocess_features(jeopardy_dataframe):
     processed_features["audacity_b"] = (
             jeopardy_dataframe["incorrect_b"] /
             jeopardy_dataframe["correct_b"])
-    processed_features["audacity_a"] = (
-            jeopardy_dataframe["incorrect_b"] /
-            jeopardy_dataframe["correct_b"])
+    processed_features["audacity_c"] = (
+            jeopardy_dataframe["incorrect_c"] /
+            jeopardy_dataframe["correct_c"])
     return processed_features
 
 
@@ -100,47 +98,12 @@ display.display(training_targets.describe())
 print("Validation targets summary:")
 display.display(validation_targets.describe())
 
+# Determine correlation between variables in order to select best features.
+correlation_dataframe = training_examples.copy()
+correlation_dataframe["target"] = training_targets["leader_win"]
 
-def construct_feature_columns(input_features):
-    """Construct the TensorFlow Feature Columns.
-
-    Args:
-     input_features: The names of the numerical input features to use.
-    Returns:
-    A set of feature columns
-  """
-
-    for my_feature in input_features:
-        if my_feature in ("correct_a",
-            "correct_b",
-            "correct_c",
-            "incorrect_a",
-            "incorrect_b",
-            "incorrect_c",
-            "num_since_correct_a",
-            "num_since_correct_b",
-            "num_since_correct_c",
-            "dd_a",
-            "dd_b",
-            "dd_c",
-            "num_wins_row_champ",
-            "amount_before_final_a",
-            "amount_before_final_b",
-            "amount_before_final_c",
-            "amount_leader_wagered"):
-            f_list = set([tf.feature_column.numeric_column(my_feature)])
-    occupation_feature_column = tf.feature_column.categorical_column_with_vocabulary_file(
-        key='occupation_leader',
-        vocabulary_file="unique_occupations.txt",
-        vocabulary_size=86)
-    f_list.union(occupation_feature_column)
-    final_category_feature_column = tf.feature_column.categorical_column_with_vocabulary_file(
-        key='final_category',
-        vocabulary_file="unique_final_jeopardy_categories.txt",
-        vocabulary_size=181)
-    f_list.union(final_category_feature_column)
-
-    return f_list
+corr = correlation_dataframe.corr()
+display.display(corr)
 
 
 def my_input_fn(features, targets, batch_size=1, shuffle=True, num_epochs=None):
@@ -172,45 +135,151 @@ def my_input_fn(features, targets, batch_size=1, shuffle=True, num_epochs=None):
     return features, labels
 
 
+def get_quantile_based_buckets(feature_values, num_buckets):
+  quantiles = feature_values.quantile(
+    [(i+1.)/(num_buckets + 1.) for i in range(num_buckets)])
+  return [quantiles[q] for q in quantiles.keys()]
+
+
+
+def construct_feature_columns():
+    """Construct the TensorFlow Feature Columns.
+
+  Returns:
+    A set of feature columns
+  """
+
+    bucketized_correct_a = tf.feature_column.bucketized_column(
+        tf.feature_column.numeric_column("correct_a"),
+        boundaries=get_quantile_based_buckets(training_examples["correct_a"], 4))
+    bucketized_correct_b = tf.feature_column.bucketized_column(
+        tf.feature_column.numeric_column("correct_b"),
+        boundaries=get_quantile_based_buckets(training_examples["correct_b"], 4))
+    bucketized_correct_c = tf.feature_column.bucketized_column(
+        tf.feature_column.numeric_column("correct_c"),
+        boundaries=get_quantile_based_buckets(training_examples["correct_c"], 4))
+    bucketized_audacity_a = tf.feature_column.bucketized_column(
+        tf.feature_column.numeric_column("audacity_a"),
+        boundaries=get_quantile_based_buckets(training_examples["audacity_a"], 4))
+    bucketized_audacity_b = tf.feature_column.bucketized_column(
+        tf.feature_column.numeric_column("audacity_b"),
+        boundaries=get_quantile_based_buckets(training_examples["audacity_b"], 4))
+    bucketized_audacity_c = tf.feature_column.bucketized_column(
+        tf.feature_column.numeric_column("audacity_c"),
+        boundaries=get_quantile_based_buckets(training_examples["audacity_c"], 4))
+    bucketized_num_since_correct_a = tf.feature_column.bucketized_column(
+        tf.feature_column.numeric_column("num_since_correct_a"),
+        boundaries=get_quantile_based_buckets(training_examples["num_since_correct_a"],4))
+    bucketized_num_since_correct_b = tf.feature_column.bucketized_column(
+        tf.feature_column.numeric_column("num_since_correct_b"),
+        boundaries=get_quantile_based_buckets(training_examples["num_since_correct_b"], 4))
+    bucketized_num_since_correct_c = tf.feature_column.bucketized_column(
+        tf.feature_column.numeric_column("num_since_correct_c"),
+        boundaries=get_quantile_based_buckets(training_examples["num_since_correct_c"], 4))
+    bucketized_amount_before_final_a = tf.feature_column.bucketized_column(
+        tf.feature_column.numeric_column("amount_before_final_a"),
+        boundaries=get_quantile_based_buckets(training_examples["amount_before_final_a"], 6))
+    bucketized_amount_before_final_b= tf.feature_column.bucketized_column(
+        tf.feature_column.numeric_column("amount_before_final_b"),
+        boundaries=get_quantile_based_buckets(training_examples["amount_before_final_b"], 6))
+    bucketized_amount_before_final_c = tf.feature_column.bucketized_column(
+        tf.feature_column.numeric_column("amount_before_final_c"),
+        boundaries=get_quantile_based_buckets(training_examples["amount_before_final_c"], 6))
+    """
+    bucketized_amount_leader_wagered = tf.feature_column.bucketized_column(
+        tf.feature_column.numeric_column("amount_leader_wagered"),
+        boundaries=get_quantile_based_buckets(training_examples["amount_leader_wagered"], 6))
+    """
+    feature_columns = set([
+        bucketized_correct_a,
+        bucketized_correct_b,
+        bucketized_correct_c,
+    bucketized_amount_before_final_a,
+    bucketized_amount_before_final_b,
+    bucketized_amount_before_final_c,
+    bucketized_audacity_a,
+    bucketized_audacity_b,
+    bucketized_audacity_c,
+    bucketized_num_since_correct_a,
+    bucketized_num_since_correct_b,
+    bucketized_num_since_correct_c])
+    feature_columns.union([tf.feature_column.numeric_column("dd_a")])
+    feature_columns.union([tf.feature_column.numeric_column("dd_b")])
+    feature_columns.union([tf.feature_column.numeric_column("dd_c")])
+    feature_columns.union([tf.feature_column.numeric_column("num_wins_row_champ")])
+    feature_columns.union([tf.feature_column.numeric_column("amount_leader_wagered")])
+    occupation_feature_column = tf.feature_column.categorical_column_with_vocabulary_file(
+        key='occupation_leader',
+        vocabulary_file="unique_occupations.txt",
+        vocabulary_size=86)
+    feature_columns.union(occupation_feature_column)
+    final_category_feature_column = tf.feature_column.categorical_column_with_vocabulary_file(
+        key='final_category',
+        vocabulary_file="unique_final_jeopardy_categories.txt",
+        vocabulary_size=181)
+    feature_columns.union(final_category_feature_column)
+
+    return feature_columns
+
+
+def model_size(estimator):
+    variables = estimator.get_variable_names()
+    size = 0
+    for variable in variables:
+        if not any(x in variable
+               for x in ['global_step',
+                         'centered_bias_weight',
+                         'bias_weight',
+                         'Ftrl']
+              ):
+          size += np.count_nonzero(estimator.get_variable_value(variable))
+    return size
+
+
 def train_linear_classifier_model(
         learning_rate,
+        regularization_strength,
         steps,
         batch_size,
+        feature_columns,
         training_examples,
         training_targets,
         validation_examples,
         validation_targets):
-    """Trains a linear classification model.
+    """Trains a linear regression model.
 
-    In addition to training, this function also prints training progress information,
-    as well as a plot of the training and validation loss over time.
+      In addition to training, this function also prints training progress information,
+      as well as a plot of the training and validation loss over time.
 
-    Args:
-      learning_rate: A `float`, the learning rate.
-      steps: A non-zero `int`, the total number of training steps. A training step
-        consists of a forward and backward pass using a single batch.
-      batch_size: A non-zero `int`, the batch size.
-      training_examples: A `DataFrame` containing one or more columns from
-        `jeopardy_dataframe` to use as input features for training.
-      training_targets: A `DataFrame` containing exactly one column from
-        `jeopardy_dataframe` to use as target for training.
-      validation_examples: A `DataFrame` containing one or more columns from
-        `jeopardy_dataframe` to use as input features for validation.
-      validation_targets: A `DataFrame` containing exactly one column from
-        `jeopardy_dataframe` to use as target for validation.
+      Args:
+        learning_rate: A `float`, the learning rate.
+        regularization_strength: A `float` that indicates the strength of the L1
+           regularization. A value of `0.0` means no regularization.
+        steps: A non-zero `int`, the total number of training steps. A training step
+          consists of a forward and backward pass using a single batch.
+        feature_columns: A `set` specifying the input feature columns to use.
+        training_examples: A `DataFrame` containing one or more columns from
+          `jeopardy_dataframe` to use as input features for training.
+        training_targets: A `DataFrame` containing exactly one column from
+          `jeopardy_dataframe` to use as target for training.
+        validation_examples: A `DataFrame` containing one or more columns from
+          `jeopardy_dataframe` to use as input features for validation.
+        validation_targets: A `DataFrame` containing exactly one column from
+          `jeopardy_dataframe` to use as target for validation.
 
-    Returns:
-      A `LinearClassifier` object trained on the training data.
-    """
+      Returns:
+        A `LinearClassifier` object trained on the training data.
+      """
 
-    periods = 10
+    periods = 7
     steps_per_period = steps / periods
 
     # Create a linear classifier object.
-    my_optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
+    my_optimizer = tf.train.FtrlOptimizer(learning_rate=learning_rate,
+                                          l1_regularization_strength=regularization_strength)
     my_optimizer = tf.contrib.estimator.clip_gradients_by_norm(my_optimizer, 5.0)
     linear_classifier = tf.estimator.LinearClassifier(
-        feature_columns=construct_feature_columns(training_examples),
+        feature_columns=feature_columns,
         optimizer=my_optimizer
     )
 
@@ -268,11 +337,14 @@ def train_linear_classifier_model(
 
 
 linear_classifier = train_linear_classifier_model(
-    learning_rate=0.000005,
-    steps=500,
-    batch_size=20,
+    learning_rate=0.1,
+    regularization_strength=0.1,
+    steps=300,
+    batch_size=100,
+    feature_columns=construct_feature_columns(),
     training_examples=training_examples,
     training_targets=training_targets,
     validation_examples=validation_examples,
     validation_targets=validation_targets)
+print("Model size:", model_size(linear_classifier))
 
